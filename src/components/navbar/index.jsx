007e5abd02fe677ebaeafdd0a10f45c6e7ec93e5
Jéssica Martins <jesscodes@proton.me>
2025-10-24 21:02:53 -0300

    Worked on context, fetching data from API, rendering data into the UI

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,36 @@
+import { NavLink } from "react-router-dom"
+import { useContext } from "react"
+import { GlobalContext } from "../../context"
+
 export default function Navbar(){
+
+    const {searchParam, setSearchParam, handleSubmit} = useContext(GlobalContext);
+
     return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap:0">
-        <h2 className="text-2xl font-semibold">Recipes</h2>
-        <form>
+        <h2 className="text-2xl font-semibold">
+            <ul>
+                <li>
+                    <NavLink to={"/"} className="text-black hover:text-gray-700 duration-300">Recipes</NavLink>
+                </li>
+            </ul>
+        </h2>
+        <form onSubmit={handleSubmit}>
             <input
             type="text"
             name="search"
+            value={searchParam}
+            onChange={(event) => setSearchParam(event.target.value)}
             placeholder="Search for term..."
             className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
             />
         </form>
+        <ul className="flex gap-5">
+            <li>
+                <NavLink to={"/"} className="text-black hover:text-gray-700 duration-300">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/favorites"} className="text-black hover:text-gray-700 duration-300">Favorites</NavLink>
+            </li>
+        </ul>
     </nav>
 }
---
+++ src/context/index.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react"
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({children}){
+
+    const [searchParam, setSearchParam] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [recipeList, setRecipeList] = useState([]);
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        try {
+            const response = await fetch (`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
+
+            const data = await response.json();
+            if (data?.data?.recipes){
+                setRecipeList(data?.data?.recipes);
+                setLoading(false);
+                setSearchParam('');
+            }
+
+            console.log(data);
+        } catch(e){
+            console.log(e);
+            setLoading(false);
+            setSearchParam('');
+        }
+    }
+
+    console.log(loading, recipeList);
+
+    return <GlobalContext.Provider value={{searchParam, loading, recipeList, setSearchParam, handleSubmit}}>{children}</GlobalContext.Provider>
+}
